e2743e33c8ba1883d922c337286e52cc347e8207
Vityarock <bvs@maxidom.ru>
2021-11-08 14:39:00 +0300

    update2

--- blog/settings.py
+++ blog/settings.py
@@ -16,7 +16,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://limitless-forest-67700.herokuapp.com/']
+ALLOWED_HOSTS = ['https://evening-waters-38537.herokuapp.com/']
 
 
 # Application definition
